f5947910d2d0008731b6af1d0941f9561b8e7b45
Geoff Gard <gardgeoff95@email.arizona.edu>
2019-07-10 22:12:26 -0700

    finished

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # liri-node-app
+
+
+###This app takes in command line arguments and gives you information based on what you input
+#There are 4 commands you can input that all accept different parameters
+
+1. spotify-this-song < song name > = outputs information about a song using spotify API
+![](https://gyazo.com/2b78eaf2274ad2f2306eb346244aa15e)
+2. concert-this < artist name > = outputs a list of concerts for a specified artist
+![](https://gyazo.com/53373638a159de5873cefe203b82ca66)
+3. movie-this < movie name > = outputs information about a certain movie using OMDB databse
+![](https://gyazo.com/9be620791d5209438a1859d0dc8f53e8)
+4. do-what-it says 
--- liri.js
+++ liri.js
@@ -1,22 +1,80 @@
+
 require("dotenv").config();
+var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var keys = require("./keys");
 
 var spotify = new Spotify(keys.spotify);
 
 var actions = process.argv[2];
-var arguments = ""
-for (i = 3; i < process.argv.length; i ++ ){
-    arguments += process.argv[i] + " ";
+var arguments = process.argv.slice(3).join(" ");
+   
+     
+  
 
+
+
+if (actions == "spotify-this-song") {
+    spotify.search({ type: "track", query: arguments }, function (err, data) {
+        if (err) {
+            return console.log("Error Occured: " + err);
+        }
+
+        var firstSong = data.tracks.items[0];
+        console.log("Song Name: " + firstSong.name);
+        console.log("Album Name: " + firstSong.album.name);
+        console.log("Artist(s): " + firstSong.artists[0].name);
+        console.log("Preview Link: "+ firstSong)
+
+
+    })
 }
-console.log(arguments);
 
 
-spotify.search({type: "artist", query: arguments.trimRight()},function(err, data){
-    if (err){
-        return console.log("Error Occured: " + err);
+if (actions == "movie-this") {
+
+    if (arguments != "") {
+        arguments = arguments.replace(/\s/g, '+');
+    } else {
+        arguments = "Mr.+Nobody"
+
+
     }
-    console.log(data);
+    axios
+        .get("http://www.omdbapi.com/?i=tt3896198&apikey=462cd14b&t=" + arguments)
+        .then(function (res) {
 
-})
+            var results = res.data;
+
+            console.log("Title: " + results.Title);
+            console.log("Year it Came Out: " + results.Released);
+            console.log("IMDB Rating: " + results.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + results.Ratings[1].Value);
+            console.log("Released in: " + results.Country);
+            console.log("Language: " + results.Language);
+            console.log("Plot: " + results.Plot);
+            console.log("Actors: " + results.Actors);
+
+
+        })
+}
+
+if (actions == "concert-this"){
+    arguments = arguments.trim().replace(/\s/g, '+');
+    debugger;
+    axios
+        .get("https://rest.bandsintown.com/artists/" + arguments + "/events?app_id=codingbootcamp")
+        .then(function(res){
+            var results =res.data;
+            
+            for (key in results){
+                console.log("----------------------------------------")
+                
+                console.log (results[key].venue.name);
+                console.log(results[key].venue.city + ", " + results[key].venue.country);
+            }
+
+        });
+
+    
+}
